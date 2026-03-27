70aea62a608592e7057005351f4bf25f9e1552be
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-27 21:27:59 +0700

    StunPowerUp

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,28 +1,58 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
     public float speed = 3f;
+    public bool isStunned = false;
     private Rigidbody rb;
     private GameObject player;
+    private Coroutine stunCountdownRoutine;
+    private float localStunEndTime = 0f;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-
+        
     }
     void Start()
     {
-
+        PlayerController p = FindObjectOfType<PlayerController>();
+        ApplyGlobalStun(p.stunEndTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isStunned) return;
         Vector3 dir = player.transform.position - transform.position;
         dir.Normalize();
         rb.AddForce(dir * speed);
     }
+
+    public void ApplyGlobalStun(float globalEndTime)
+    {
+        float remaining = globalEndTime - Time.time;
+
+        if (remaining <= 0) return;
+
+        if (stunCountdownRoutine != null)
+        {
+            StopCoroutine(stunCountdownRoutine);
+        }
+
+        stunCountdownRoutine = StartCoroutine(StunCountdown(remaining));
+    }
+
+    IEnumerator StunCountdown(float duration)
+    {
+        isStunned = true;
+        rb.linearVelocity = Vector3.zero;
+        yield return new WaitForSeconds(duration);
+        isStunned = false;
+    }
+
+        
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public Transform focalPoint;
+    public GameObject powerUpIndicator;
 
     private Rigidbody rb;
 
@@ -14,8 +15,9 @@ public class PlayerController : MonoBehaviour
     private InputAction breakAction;
 
     public bool hasPowerUp = false;
-
     private Coroutine CountDownRoutine;
+    public float stunEndTime = 0f;
+    public float stunDuration = 5f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +27,12 @@ public class PlayerController : MonoBehaviour
         moveAction = InputSystem.actions.FindAction("Move");
         smashAction = InputSystem.actions.FindAction("Smash");
         breakAction = InputSystem.actions.FindAction("Break");
+        
+    }
+
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -36,6 +44,9 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = Vector3.zero;
         }
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -56,14 +67,27 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PowerUp"))
         {
+            powerUpIndicator.SetActive(true);
             hasPowerUp = true;
             Destroy(other.gameObject);
 
-            if(CountDownRoutine != null)
+            if (CountDownRoutine != null)
             {
-                StopCoroutine(PowerUpCountDown());
+                StopCoroutine(CountDownRoutine);
             }
             CountDownRoutine = StartCoroutine(PowerUpCountDown());
+        }
+        
+        if (other.gameObject.CompareTag("StunPowerUp"))
+        {
+            stunEndTime = Time.time + stunDuration;
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+            foreach (Enemy e in enemies)
+            {
+                e.ApplyGlobalStun(stunEndTime);
+            }
+
+            Destroy(other.gameObject);
         }
     }
 
@@ -72,6 +96,9 @@ public class PlayerController : MonoBehaviour
     IEnumerator PowerUpCountDown()
     {
         yield return new WaitForSeconds(10);
+        powerUpIndicator.SetActive(false);
         hasPowerUp = false;
     }
+
+    
 }
